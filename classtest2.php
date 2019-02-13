ddbaf1e1caa27f08ad1f99f935aed7d483f74b82
gin <1668940178@qq.com>
2019-02-13 17:04:01 +0800

    '~.~'

---
+++ classtest2.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: v_jieyshen
+ * Date: 2019/2/13
+ * Time: 11:46
+ */
+
+namespace ginforerver;
+
+class classtest2
+{
+    static public function test1()
+    {
+        echo 'gingignffffff';
+    }
+}
