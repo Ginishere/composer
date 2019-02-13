2311e3e88e54fad8ef2d3181e8264c7af30c3f16
Ginishere <945035919@qq.com>
2019-02-13 12:36:13 +0800

    first

---
+++ lib/classtest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: v_jieyshen
+ * Date: 2019/2/13
+ * Time: 11:46
+ */
+
+namespace gin;
+
+class classtest
+{
+    static public function test1()
+    {
+        echo 11112222;
+    }
+}
