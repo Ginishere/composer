e8dc1214c0b4b65b4bd3e8ee452eed049ddf355a
gin <1668940178@qq.com>
2019-02-18 09:57:42 +0800

    'version-0.0.1'

---
+++ classtest3.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: v_jieyshen
+ * Date: 2019/2/13
+ * Time: 11:46
+ */
+
+namespace gin;
+
+class classtest3
+{
+    static public function test1()
+    {
+        echo 'new composer version';
+    }
+}
