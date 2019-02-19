dc49722c20cb60b52ccd0b74c8bf5dc8d7a7f31e
gin <1668940178@qq.com>
2019-02-19 09:46:53 +0800

    '444444'

---
+++ classtest4.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: v_jieyshen
+ * Date: 2019/2/13
+ * Time: 11:46
+ */
+
+namespace gin;
+
+class classtest4
+{
+    static public function test1()
+    {
+        echo 'clasesss4444';
+    }
+}
